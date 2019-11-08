2fc6d46d56da37873871639d20ca5de881245dfd
vido21 <vidobae@gmail.com>
2019-11-08 19:03:43 +0700

    tambah dikit

--- main.py
+++ main.py
@@ -1,42 +1,70 @@
 from model import Model
 import random
+from itertools import combinations
 
-def inisiasiPopulasi(max):
+def inisiasiPopulasi(maxPopulasi,panjangKromosom):
     populasi=[]
     for i in range(max):
-        randomKromosom=[random.randint(1,6),
-                        random.randint(1,10),
-                        ]
+        randomKromosom=[random.randint(1,6) for i in range(panjangKromosom)]
         populasi.append({"kromosom":randomKromosom,"fitness":0})
     return populasi
-def seleksi(populasi):
+
+def seleksi(populasi,pilih):
     matingPool=[]
-    for i in range(3):
+    for i in range(pilih):
         a=random.choice(populasi)
         b=random.choice(populasi)
         if (a['fitness']>b['fitness']):
             matingPool.append(a)
         else:
             matingPool.append(b)
+    return matingPool
 
-# def crossOver(populasi):
+def crossOver(populasi):
+    offspring=[]
+    tmp=combinations(populasi,2)
+    # masih belum jadi
 
-# def mutasi()
+def mutasi(populasi):
+    panjangKromosom=9
+    individu=populasi.index(findchoose.random(populasi))
+    gen=random.randint(0,panjangKromosom+1)
+    populasi[individu]['kromosom'][gen]=random.randint(1,255)
+    m=Model(populasi[individu]['kromosom'])
+    m.train()
+    populasi[individu]['fitness']=m.evaluate()
+    del m
 
 def fitness(populasi):
     for individu in range(len(populasi)):
         m=Model(populasi[individu]['kromosom'])
         m.train()
         populasi[individu]['fitness']=m.evaluate()
+        del m
 
 if __name__=="__main__":
+    #inisiasi Populasi
     populasi=inisiasiPopulasi(5)
-    print(populasi)
-    fitness(populasi)
-    seleksi()
 
-    # generation=0
-    # while generation<5:
+    for i in range(5):
+        #menghitung fitness
+        fitness(populasi)
+        
+        #seleksi
+        populasi=seleksi(populasi,3)
+        print(populasi)
+
+        #crossover
+        crossOver(populasi)
+        fitness(populasi)
+
+        #mutasi
+        mutasi(populasi)
+
+    
+    print populasi
+        
+
     
     
     # totalPopulasi=[]
