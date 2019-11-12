15a82c8cde3bdb430b4981ba09a45dbf968177b6
apikdech <apikdech@gmail.com>
2019-11-12 12:56:09 +0700

    [ricky] add crossover function

--- main.py
+++ main.py
@@ -20,10 +20,34 @@ def seleksi(populasi,pilih):
             matingPool.append(b)
     return matingPool
 
-def crossOver(populasi):
-    offspring=[]
-    tmp=combinations(populasi,2)
-    # masih belum jadi
+def one_point_crossover(x, y):
+    # crossover on position 4
+    x = "{0:b}".format(x).zfill(9)
+    y = "{0:b}".format(y).zfill(9)
+    cx = int(x[:4] + y[4:], 2)
+    cy = int(y[:4] + x[4:], 2)
+    return (cx, cy)
+
+def crossover(populasi):
+    offspring = []
+    tmp = combinations(populasi, 2)
+    x = []
+    for _ in tmp:
+        x.append(_)
+    random.shuffle(x)
+    # Ambil n/2 dari nC2 populasi
+    x = x[:len(populasi)//2]
+    for p1, p2 in x:
+        tx = []
+        ty = []
+        for index in range(len(p1)):
+            result = one_point_crossover(p1[index], p2[index])
+            tx.append(result[0])
+            ty.append(result[1])
+        offspring.extend((tx, ty))
+    offspring.extend(populasi)
+    # hasilnya akan terdapat sebanyak 2x|populasi| awal
+    return offspring
 
 def mutasi(populasi):
     panjangKromosom=9
